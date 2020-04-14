79274e434451cd753c13a58ee05a79fa15fce3b3
Chris Burge <topher.burge@gmail.com>
2020-04-14 12:47:31 -0700

    finish parsing body code and add error handling.

--- app.js
+++ app.js
@@ -14,12 +14,17 @@ const server = http.createServer((req, res) => {
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
-      body.push();
+      body.push(chunk);
     });
-    fs.writeFileSync('message.txt', 'DUMMY');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
