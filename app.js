ed80e389cc3daa708757c71624db7f90081cfd6b
Chris Burge <topher.burge@gmail.com>
2020-04-20 10:49:16 -0700

    add form for input.

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the MiddleEarth')
-  next(); // Allows the request to continue on to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+	res.send('<form action="/product" method="POST"><input type="text" name="title"><buttom type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-  console.log("In the MiddleWare");
+app.get('/product', (req, res, next) => {
+  console.log(res.body)
+  res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello From Express.js!</h1>');
 });
 
