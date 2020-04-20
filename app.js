60028cec8763c4801631ee38f659ada62f2839e8
Chris Burge <topher.burge@gmail.com>
2020-04-20 10:21:50 -0700

    add middleware statements with express

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('In the MiddleEarth')
+  next(); // Allows the request to continue on to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In the MiddleWare");
+  res.send('<h1>Hello From Express.js!</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(7000);
