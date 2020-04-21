65f1a3f0026027db63c2c4bcf6df2f1b82d77acc
Chris Burge <topher.burge@gmail.com>
2020-04-20 20:06:38 -0700

    serve static css pages to views.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const rootDir = require("..util/path");
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', 'error404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', 'error404.html'));
 });
 
 app.listen(3000);
